f7c02c7bc4d40a25408cbfca432951d66e8f2b01
Assem <asemnurzankyzy6@gmail.com>
2026-02-10 10:53:12 +0500

    lab3

---
+++ lab3/task1/JSFundamentals/variable.js
@@ -0,0 +1,19 @@
+let admin, name;
+
+name = "John";
+
+admin = name;
+
+alert( admin );
+
+
+
+let solarSystemHumansPlanet = "Earth";
+
+let currentUserName = "Luna";
+
+
+
+const BIRTHDAY = '18.04.1982';
+
+const age = someCode(birthday);
